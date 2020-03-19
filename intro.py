654cc076300a0eeb3adbf90c902cd2ad6f95327c
Leonardo Patrick Luna Palomino <lplpalomino@gmail.com>
2020-03-19 00:15:46 -0500

    Archivos iniciales

---
+++ intro.py
@@ -0,0 +1,15 @@
+#Variables
+variable = 'Hola mundo'
+variable2 = "Hello world"
+x = 15
+y = 9.4
+bool = False
+bool2 = True
+variable = 12
+
+#Output
+print(variable2)
+
+#Input
+edad = input('Ingresa tu edad: ')
+print('Tu edad es:', edad)
---
+++ suma.py
@@ -0,0 +1,6 @@
+num1 = int(input('Ingresa valor 1: '))
+num2 = int(input('Ingresa valor 2: '))
+
+suma = num1 + num2
+
+print('La suma es:',suma)
